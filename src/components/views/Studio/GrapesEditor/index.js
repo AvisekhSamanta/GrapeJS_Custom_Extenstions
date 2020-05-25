88723d1af9e724c44c5432abeaa2a43093234b28
Avisekh Samanta <avisekh.samanta@ericsson.com>
2020-05-25 11:59:03 +0530

    second commit

--- src/components/views/Studio/GrapesEditor/index.js
+++ src/components/views/Studio/GrapesEditor/index.js
@@ -31,6 +31,29 @@ export default function GrapesEditor(props) {
 			// Get the content for the canvas directly from the element
 			// As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
 			fromElement: true,
+			assetManager: {
+				dropzone: 1,
+				dropzoneContent: '<div class="dropzone-inner"> Drop here your assets</div>',
+				assets: [
+				 'http://placehold.it/350x250/78c5d6/fff/image1.jpg',
+				 // Pass an object with your properties
+				 {
+				   type: 'image',
+				   src: 'http://placehold.it/350x250/459ba8/fff/image2.jpg',
+				   height: 350,
+				   width: 250
+				 },
+				 {
+				   // As the 'image' is the base type of assets, omitting it will
+				   // be set as `image` by default
+				   src: 'http://placehold.it/350x250/79c267/fff/image3.jpg',
+				   height: 350,
+				   width: 250
+				 },  
+					
+				  
+				],
+			  },
 			// Disable the storage manager for the moment
 			storageManager: false,
 			blockManager: {
@@ -59,6 +82,7 @@ export default function GrapesEditor(props) {
 						// This triggers `active` event on dropped components and the `image`
 						// reacts by opening the AssetManager
 						activate: true,
+					
 					}
 				]
 			},
